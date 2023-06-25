eaf23c92b158e6f9467cb9a1d355244e8ba81fde
Hui Chen <hchen5796@gmail.com>
2023-06-25 21:49:28 +0200

    done upload, todo file deleter

--- frontend_cryo/src/app/unit-data-upload/unit-data-upload.component.ts
+++ frontend_cryo/src/app/unit-data-upload/unit-data-upload.component.ts
@@ -1,12 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FileTransferService } from '../app-services';
-import { NestedTreeControl } from '@angular/cdk/tree';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
-
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
 
 @Component({
   selector: 'app-unit-data-upload',
@@ -14,58 +7,23 @@ interface FoodNode {
   styleUrls: ['./unit-data-upload.component.css']
 })
 export class UnitDataUploadComponent {
-  TREE_DATA: FoodNode[] = [
-    {
-      name: 'Fruit',
-      children: [{ name: 'Apple' }, { name: 'Banana' }, { name: 'Fruit loops' }],
-    },
+  uploadedFiles: {file_name:string, result:string}[] = [];
+  @Input() onlyDir!: boolean;
+  @Input() allowMultiple!: boolean;
+  @Input() allowFolder!: boolean;
+  @Input() data_type!: 'pre_data' | 'post_data' | 'cpa' | 'exp' | 'process';
+
+  onFileSelected(event: any) {
+    const files: FileList = event.target.files;
+    if (files.length > 0) {
+      this.fileTransferService.fileUpload(files, this.data_type).then((res) => { this.uploadedFiles = JSON.parse(res.replace(/'/g, '"'))})
+    }
+  }
+
+  constructor(
+    private fileTransferService: FileTransferService
+  ) {
     
-  ];
-  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<FoodNode>();
-
-  selectedFile: File | null = null;
-
-  constructor(private fileTransferService: FileTransferService) { this.dataSource.data = this.TREE_DATA; }
-
-  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
-
-  extensionsLow = {
-    'pre_data': ['txt', 'csv', 'asc'],
-    'post_data': ['txt', 'csv', 'asc'],
-    'process': ['txt', 'csv', 'asc'],
-    'exp': ['txt', 'csv', 'asc'],
-    'cpa': ['txt', 'csv', 'asc'],
   }
 
-  onFileSelected(event: any, data_type: 'pre_data' | 'post_data' | 'cpa' | 'exp' | 'process'): void {
-    this.selectedFile = event.target.files[0];
-    if (this.selectedFile) {
-      if ((this.extensionsLow[data_type]).indexOf(getFileExtension(this.selectedFile.name)) != -1) {
-        console.log(`right format: ${getFileExtension(this.selectedFile.name)}`)
-      } else {
-        console.log('wrong file format')
-      }
-
-    } else {
-      console.log('nothing selected')
-    }
-  }
-
-  uploadFile(): void {
-    if (this.selectedFile) {
-      this.fileTransferService.fileUpload(this.selectedFile, 'process').then((res) => { console.log(res) })
-    } else {
-      console.log('nothing uploaded')
-    }
-  }
-}
-
-function getFileExtension(fileName: string): string {
-  const dotIndex = fileName.lastIndexOf('.');
-  if (dotIndex === -1) {
-    return 'error';
-  }
-  const extension = fileName.slice(dotIndex + 1).toLowerCase();
-  return extension;
 }
