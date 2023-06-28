6413fd310b10900c2858fab6f96c547429d042ff
Hui Chen <hchen5796@gmail.com>
2023-06-28 18:56:29 +0200

    done

---
+++ frontend_cryo/src/app/seite-data-storage/seite-data-storage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seite-data-storage',
+  templateUrl: './seite-data-storage.component.html',
+  styleUrls: ['./seite-data-storage.component.css']
+})
+export class SeiteDataStorageComponent {
+
+}
