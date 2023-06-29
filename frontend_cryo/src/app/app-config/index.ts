8f44ee0e0c0e82ffb9093869c0c27d58365b19e5
Hui Chen <hchen5796@gmail.com>
2023-06-29 15:56:45 +0200

    Update index.ts

--- frontend_cryo/src/app/app-config/index.ts
+++ frontend_cryo/src/app/app-config/index.ts
@@ -1,5 +1,5 @@
-// export const backendUrl: string = 'http://127.0.0.1:8000'
-export const backendUrl: string = 'http://ec2-18-197-31-138.eu-central-1.compute.amazonaws.com:8000'
+export const backendUrl: string = 'http://127.0.0.1:8000'
+// export const backendUrl: string = 'http://3.72.113.129:8000'
 export const dataStoreName: string = 'data_store'
 
 export interface CpaStructur {
