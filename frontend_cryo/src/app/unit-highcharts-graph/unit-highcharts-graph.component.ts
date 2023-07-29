311d786c2aa83b77e4a63ccb998d3fe431468014
Hui Chen <hchen5796@gmail.com>
2023-07-29 22:11:05 +0200

    done analsey experiemnt

--- frontend_cryo/src/app/unit-highcharts-graph/unit-highcharts-graph.component.ts
+++ frontend_cryo/src/app/unit-highcharts-graph/unit-highcharts-graph.component.ts
@@ -14,13 +14,26 @@ export class UnitHighchartsGraphComponent implements OnChanges, OnInit {
   selectItem: string[] = ["Viability_(%)", "Total_cells_/_ml_(x_10^6)", "Total_viable_cells_/_ml_(x_10^6)", "Average_diameter_(microns)", "Average_circularity"]
   selectedItem: string = this.selectItem[0]
   type: any = 'column'
-  chartOptions!: Highcharts.Options
-  show: boolean = false
   Highcharts: typeof Highcharts = Highcharts;
 
+  chartOptions!: Highcharts.Options
   dataStorage: { [key: string]: { preData: any, postData: any } } = {} //number | string[]
   categories: string[] = []
-  plotBands: {from: number,to: number,color:string,label: {text: string}}[] = []
+  plotBands: { from: number, to: number, color: string, label: { text: string } }[] = []
+  show: boolean = false
+  classColors: { [key: string]: string } = {}
+
+  chartOptions_average_versuch!: Highcharts.Options
+  dataStorage_average_versuch: { [key: string]: { preData: any, postData: any } } = {} //number | string[]
+  categories_average_versuch: string[] = []
+  plotBands_average_versuch: { from: number, to: number, color: string, label: { text: string } }[] = []
+  show_average_versuch: boolean = false
+
+  chartOptions_average_probe!: Highcharts.Options
+  dataStorage_average_probe: { [key: string]: { preData: any, postData: any } } = {} //number | string[]
+  categories_average_probe: string[] = []
+  plotBands_average_probe: { from: number, to: number, color: string, label: { text: string } }[] = []
+  show_average_probe: boolean = false
 
   constructor(
     private queryNeo4jService: QueryNeo4jService,
@@ -29,9 +42,17 @@ export class UnitHighchartsGraphComponent implements OnChanges, OnInit {
   ngOnInit(): void {
     HC_more(Highcharts)
     HC_accessibility(Highcharts);
+    this.classColors = {}
   }
 
   ngOnChanges(): void {
+    this.ngOnInit()
+    this.loadChartOptions()
+    this.loadAverageNachVersuch()
+    this.loadAverageNachProbe()
+  }
+
+  loadChartOptions() {
     if (this.experiment) {
       this.experiment['child'].forEach((versuch: any) => {
         versuch['probes'].forEach((probe: any) => {
@@ -41,10 +62,13 @@ export class UnitHighchartsGraphComponent implements OnChanges, OnInit {
       this.categories = this.getObjectKeys(this.dataStorage)
       const pB: string[] = this.getObjectKeys(this.dataStorage).map(Unique_ID => Unique_ID.split('*-*')[1])
 
-      findIndexRanges(pB).forEach((range:any, element:any)=>{
-        this.plotBands.push({from: range[0] -0.5,to: range[1] + 0.5,color:getRandomCoolColor(), label: {text: element}})
+      findIndexRanges(pB).forEach((range: any, element: any) => {
+        if (!this.classColors[element]) {
+          this.classColors[element] = getRandomCoolColor()
+        }
+        this.plotBands.push({ from: range[0] - 0.5, to: range[1] + 0.5, color: this.classColors[element], label: { text: element } })
       })
-      
+
       let index = 0
       this.categories.forEach((probe_id: string) => {
         this.queryNeo4jService.buildColumn(this.dataStorage[probe_id]['preData'], this.dataStorage[probe_id]['postData'], this.selectedItem).then((res: any) => {
@@ -53,9 +77,9 @@ export class UnitHighchartsGraphComponent implements OnChanges, OnInit {
           const pre_SE = parseFloat(res['pre_data']['SE'])
           const post_SE = parseFloat(res['post_data']['SE'])
           this.dataStorage[probe_id] = { preData: { mean: pre_mean, SE: pre_SE }, postData: { mean: post_mean, SE: post_SE } }
-          index +=1
+          index += 1
           if (index === this.categories.length) {
-            this.update()
+            this.updateChartOptions()
             this.show = true
           }
         })
@@ -63,13 +87,13 @@ export class UnitHighchartsGraphComponent implements OnChanges, OnInit {
     }
   }
 
-  update() {
+  updateChartOptions() {
     this.chartOptions = {
       title: {
         text: this.experiment['experiment']['Experiment_ID']
       },
       xAxis: {
-        categories: this.categories.map(name=>name.split('*-*')[2]),
+        categories: this.categories.map(name => name.split('*-*')[2]),
         plotBands: this.plotBands,
       },
       yAxis: {
@@ -110,6 +134,187 @@ export class UnitHighchartsGraphComponent implements OnChanges, OnInit {
       ]
     };
   }
+
+  loadAverageNachVersuch() {
+    if (this.experiment) {
+      this.experiment['child'].forEach((versuch: any) => {
+        let preData_list: any[] = []
+        let postData_list: any[] = []
+        versuch['probes'].forEach((probe: any) => {
+          preData_list = preData_list.concat(probe['PreData_ID'])
+          postData_list = postData_list.concat(probe['PostData_ID'])
+        })
+        this.dataStorage_average_versuch[versuch['versuch']['Versuch_ID']] = { preData: preData_list, postData: postData_list }
+      })
+
+      this.categories_average_versuch = this.getObjectKeys(this.dataStorage_average_versuch)
+
+      findIndexRanges(this.categories_average_versuch).forEach((range: any, element: any) => {
+        this.plotBands_average_versuch.push({ from: range[0] - 0.5, to: range[1] + 0.5, color: this.classColors[element], label: { text: '' } })
+      })
+
+      let index = 0
+      this.categories_average_versuch.forEach((versuch_id: string) => {
+        this.queryNeo4jService.buildColumn(this.dataStorage_average_versuch[versuch_id]['preData'], this.dataStorage_average_versuch[versuch_id]['postData'], this.selectedItem).then((res: any) => {
+          const pre_mean = parseFloat(res['pre_data']['mean'])
+          const post_mean = parseFloat(res['post_data']['mean'])
+          const pre_SE = parseFloat(res['pre_data']['SE'])
+          const post_SE = parseFloat(res['post_data']['SE'])
+          this.dataStorage_average_versuch[versuch_id] = { preData: { mean: pre_mean, SE: pre_SE }, postData: { mean: post_mean, SE: post_SE } }
+          index += 1
+          if (index === this.categories_average_versuch.length) {
+            this.updateAverageNachVersuch()
+            this.show_average_versuch = true
+          }
+        })
+      })
+    }
+  }
+
+  updateAverageNachVersuch() {
+    this.chartOptions_average_versuch = {
+      title: {
+        text: this.experiment['experiment']['Experiment_ID']
+      },
+      xAxis: {
+        categories: this.categories_average_versuch,
+        plotBands: this.plotBands_average_versuch,
+      },
+      yAxis: {
+        title: {
+          text: this.selectedItem
+        }
+      },
+      // tooltip: {
+      //   valueSuffix: ' (1000 MT)'
+      // },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: [
+        {
+          name: 'pre data',
+          type: this.type,
+          data: this.categories_average_versuch.map(probeId => this.dataStorage_average_versuch[probeId]['preData']['mean'])
+        },
+        {
+          name: 'pre data error',
+          type: 'errorbar',
+          data: this.categories_average_versuch.map(probeId => [this.dataStorage_average_versuch[probeId]['preData']['mean'] - 1.96 * this.dataStorage_average_versuch[probeId]['preData']['SE'], this.dataStorage_average_versuch[probeId]['preData']['mean'] + 1.96 * this.dataStorage_average_versuch[probeId]['preData']['SE']])
+        },
+        {
+          name: "post data",
+          type: this.type,
+          data: this.categories_average_versuch.map(probeId => this.dataStorage_average_versuch[probeId]['postData']['mean'])
+        },
+        {
+          name: 'post data error',
+          type: 'errorbar',
+          data: this.categories_average_versuch.map(probeId => [this.dataStorage_average_versuch[probeId]['postData']['mean'] - 1.96 * this.dataStorage_average_versuch[probeId]['postData']['SE'], this.dataStorage_average_versuch[probeId]['postData']['mean'] + 1.96 * this.dataStorage_average_versuch[probeId]['postData']['SE']])
+        }
+      ]
+    };
+  }
+
+  loadAverageNachProbe() {
+    if (this.experiment) {
+      this.dataStorage_average_probe = {}
+      let dS: { [key: string]: { preData: any, postData: any } } = {}
+      this.experiment['child'].forEach((versuch: any) => {
+        versuch['probes'].forEach((probe: any) => {
+          dS[probe['Unique_ID']] = { preData: probe['PreData_ID'], postData: probe['PostData_ID'] }
+        })
+      })
+
+      for (const key in dS) {
+        const newKey = key.split('*-*')[2]
+        if (newKey) {
+          if (!this.dataStorage_average_probe[newKey]) {
+            this.dataStorage_average_probe[newKey] = { preData: [], postData: [] };
+          }
+          this.dataStorage_average_probe[newKey]['preData'] = this.dataStorage_average_probe[newKey]['preData'].concat(dS[key].preData);
+          this.dataStorage_average_probe[newKey]['postData'] = this.dataStorage_average_probe[newKey]['postData'].concat(dS[key].postData);
+        }
+      }
+
+      this.categories_average_probe = this.getObjectKeys(this.dataStorage_average_probe)
+
+      findIndexRanges(this.categories_average_probe).forEach((range: any, element: any) => {
+        if (!this.classColors[element]) {
+          this.classColors[element] = getRandomCoolColor()
+        }
+        this.plotBands_average_probe.push({ from: range[0] - 0.5, to: range[1] + 0.5, color: this.classColors[element], label: { text: '' } })
+      })
+
+      let index = 0
+      this.categories_average_probe.forEach((versuch_id: string) => {
+        this.queryNeo4jService.buildColumn(this.dataStorage_average_probe[versuch_id]['preData'], this.dataStorage_average_probe[versuch_id]['postData'], this.selectedItem).then((res: any) => {
+          const pre_mean = parseFloat(res['pre_data']['mean'])
+          const post_mean = parseFloat(res['post_data']['mean'])
+          const pre_SE = parseFloat(res['pre_data']['SE'])
+          const post_SE = parseFloat(res['post_data']['SE'])
+          this.dataStorage_average_probe[versuch_id] = { preData: { mean: pre_mean, SE: pre_SE }, postData: { mean: post_mean, SE: post_SE } }
+          index += 1
+          if (index === this.categories_average_probe.length) {
+            this.updateAverageNachProbe()
+            this.show_average_probe = true
+          }
+        })
+      })
+    }
+  }
+
+  updateAverageNachProbe() {
+    this.chartOptions_average_probe = {
+      title: {
+        text: this.experiment['experiment']['Experiment_ID']
+      },
+      xAxis: {
+        categories: this.categories_average_probe,
+        plotBands: this.plotBands_average_probe,
+      },
+      yAxis: {
+        title: {
+          text: this.selectedItem
+        }
+      },
+      // tooltip: {
+      //   valueSuffix: ' (1000 MT)'
+      // },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: [
+        {
+          name: 'pre data',
+          type: this.type,
+          data: this.categories_average_probe.map(probeId => this.dataStorage_average_probe[probeId]['preData']['mean'])
+        },
+        {
+          name: 'pre data error',
+          type: 'errorbar',
+          data: this.categories_average_probe.map(probeId => [this.dataStorage_average_probe[probeId]['preData']['mean'] - 1.96 * this.dataStorage_average_probe[probeId]['preData']['SE'], this.dataStorage_average_probe[probeId]['preData']['mean'] + 1.96 * this.dataStorage_average_probe[probeId]['preData']['SE']])
+        },
+        {
+          name: "post data",
+          type: this.type,
+          data: this.categories_average_probe.map(probeId => this.dataStorage_average_probe[probeId]['postData']['mean'])
+        },
+        {
+          name: 'post data error',
+          type: 'errorbar',
+          data: this.categories_average_probe.map(probeId => [this.dataStorage_average_probe[probeId]['postData']['mean'] - 1.96 * this.dataStorage_average_probe[probeId]['postData']['SE'], this.dataStorage_average_probe[probeId]['postData']['mean'] + 1.96 * this.dataStorage_average_probe[probeId]['postData']['SE']])
+        }
+      ]
+    };
+  }
+
   getObjectKeys(obj: any): string[] {
     if (Object.keys(obj).length === 0) {
       return []
@@ -140,12 +345,12 @@ function findIndexRanges(arr: any[]): Map<any, [number, number]> {
 
 function getRandomCoolColor(): string {
   const blueRange = 10;
-  const greenRange = 10; 
-  const purpleRange = 10; 
+  const greenRange = 10;
+  const purpleRange = 10;
 
-  const blue = (Math.floor((0.8 + Math.random() * 0.2) * (255-blueRange))).toString(16).padStart(2, '0');
-  const green = (Math.floor((0.8 + Math.random() * 0.2) * (255-greenRange))).toString(16).padStart(2, '0');
-  const purple = (Math.floor((0.8 + Math.random() * 0.2) * (255-purpleRange))).toString(16).padStart(2, '0');
+  const blue = (Math.floor((0.8 + Math.random() * 0.2) * (255 - blueRange))).toString(16).padStart(2, '0');
+  const green = (Math.floor((0.8 + Math.random() * 0.2) * (255 - greenRange))).toString(16).padStart(2, '0');
+  const purple = (Math.floor((0.8 + Math.random() * 0.2) * (255 - purpleRange))).toString(16).padStart(2, '0');
 
   return `#${blue}${green}${purple}`;
 }
