0a6c97670475a902a7683225a470fa86663357a8
Hui Chen <hchen5796@gmail.com>
2023-08-14 15:46:50 +0800

    ---

--- frontend_cryo/src/app/seite-start/seite-start.component.ts
+++ frontend_cryo/src/app/seite-start/seite-start.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-seite-start',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./seite-start.component.css']
 })
 export class SeiteStartComponent {
+  currentSubRoute: string = 'none';
+  myObserver: any
 
+  constructor(private router: Router,) { 
+    this.myObserver = (this.router.events.pipe(filter((event: any) => event instanceof NavigationEnd)) as Observable<NavigationEnd>).subscribe((router: any) => {
+      // console.log('------')
+        this.currentSubRoute = router.urlAfterRedirects
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.myObserver.unsubscribe()
+  }
 }
--- frontend_cryo/src/app/seite-welcome/seite-welcome.component.ts
+++ frontend_cryo/src/app/seite-welcome/seite-welcome.component.ts
@@ -6,7 +6,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./seite-welcome.component.css']
 })
 export class SeiteWelcomeComponent {
-  @Input() side:boolean = false
+  @Input() side: boolean = false
+  @Input() highlight: string = 'none'
   resourcesCards: { href: string, icon: string, span: string }[] = [
     { href: '/about-us', icon: 'perm_identity', span: 'About Us' },
     { href: '/about-this', icon: 'help_outline', span: 'About This' },
@@ -16,10 +17,14 @@ export class SeiteWelcomeComponent {
   ]
 
   functionCards: { icon: string, span: string, href: string }[] = [
-    { icon: 'storage', span: 'Data storage', href:'/start/data-storage' },
-    { icon: 'insert_chart_outlined', span: 'Data analyse', href:'/start/data-analyse' },
-    { icon: 'bubble_chart', span: 'Survivalrate predict', href:'/start/data-storage' },
-    { icon: 'trending_up', span: 'Parameter optimize', href:'/start/data-storage' },
-    { icon: 'favorite_border', span: 'Feedback', href:'/start/data-storage' }
+    { icon: 'storage', span: 'Data storage', href: '/start/data-storage' },
+    { icon: 'insert_chart_outlined', span: 'Data analyse', href: '/start/data-analyse' },
+    { icon: 'bubble_chart', span: 'Survivalrate predict', href: '/start/survivalrate-predict' },
+    { icon: 'trending_up', span: 'Parameter optimize', href: '/start/parameter-optimize' },
+    { icon: 'favorite_border', span: 'Feedback', href: '/start/feedback' }
   ]
+
+  check(input:string){
+    return input === this.highlight
+  }
 }
