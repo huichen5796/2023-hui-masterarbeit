a1dc243a288b96f8de26ad2391d3bb513c260abc
Hui Chen <hchen5796@gmail.com>
2024-01-10 02:25:07 +0100

    Update seite-welcome.component.ts

--- frontend_cryo/src/app/seite-welcome/seite-welcome.component.ts
+++ frontend_cryo/src/app/seite-welcome/seite-welcome.component.ts
@@ -9,20 +9,20 @@ export class SeiteWelcomeComponent {
   @Input() side: boolean = false
   @Input() highlight: string = 'none'
   resourcesCards: { href: string, icon: string, span: string }[] = [
-    { href: '/about-us', icon: 'perm_identity', span: 'About Us' },
-    { href: '/about-this', icon: 'help_outline', span: 'About This' },
-    { href: '/technology', icon: 'developer_board', span: 'Technology' },
-    { href: '/database', icon: 'cloud_queue', span: 'Database' },
-    { href: '/nn', icon: 'blur_on', span: 'Neural Networks' }
+    { href: 'about-us', icon: 'perm_identity', span: 'About Us' },
+    { href: 'about-this', icon: 'help_outline', span: 'About This' },
+    { href: 'technology', icon: 'developer_board', span: 'Technology' },
+    { href: 'database', icon: 'cloud_queue', span: 'Database' },
+    { href: 'nn', icon: 'blur_on', span: 'Neural Networks' }
   ]
 
   functionCards: { icon: string, span: string, href: string }[] = [
-    { icon: 'storage', span: 'Data storage', href: '/start/data-storage' },
-    { icon: 'insert_chart_outlined', span: 'Data analyse', href: '/start/data-analyse' },
-    { icon: 'center_focus_weak', span: 'Cells count', href: '/start/cells-count' },
-    { icon: 'bubble_chart', span: 'Survivalrate predict', href: '/start/survivalrate-predict' },
-    { icon: 'trending_up', span: 'Parameter optimize', href: '/start/parameter-optimize' },
-    { icon: 'favorite_border', span: 'Feedback', href: '/start/feedback' }
+    { icon: 'storage', span: 'Data storage', href: 'start/data-storage' },
+    { icon: 'insert_chart_outlined', span: 'Data analyse', href: 'start/data-analyse' },
+    { icon: 'center_focus_weak', span: 'Cells count', href: 'start/cells-count' },
+    { icon: 'bubble_chart', span: 'Survivalrate predict', href: 'start/survivalrate-predict' },
+    { icon: 'trending_up', span: 'Parameter optimize', href: 'start/parameter-optimize' },
+    { icon: 'favorite_border', span: 'Feedback', href: 'start/feedback' }
   ]
 
   check(input:string){
